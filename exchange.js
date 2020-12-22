370829450fa7fec2d6cbbaae4de4a4aa093ba08a
weixiangbeak <s10206320@connect.np.edu.sg>
2020-12-22 22:40:26 +0800

    fetched data from exchange currency api

---
+++ exchange.js
@@ -0,0 +1,7 @@
+fetch('http://data.fixer.io/api/latest?access_key=91b8b58aa17e185bec0821d38d28785f')
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+$(document).ready(function() {
+    
+})
